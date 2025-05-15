f1489bf714894770f7f64ae7b977a2764725f1c4
Saeed Nasser <90267986+sdnssr1@users.noreply.github.com>
2025-05-15 00:09:06 -0700

    Update build configuration and title

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import path from "path";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { tempo } from "tempo-devtools/dist/vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  base: "./",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
